0dde23670bf64809e9a571f9f1a74c5fb94ed866
benitorubano1992 <rubanobenito@gmail.com>
2023-01-19 13:55:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "pagina vue con bootstrap and sass"
+    }
+  },
+  methods: {},
+  created() {
+
+  }
+}
+</script>
+<template>
+  <h1>{{ title }}</h1>
+</template>
+<style  lang="scss">
+@use "./styles/general.scss" as *;
+</style>
